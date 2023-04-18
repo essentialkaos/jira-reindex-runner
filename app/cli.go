9f7723ce05108184a304277b44d6656fe1f465d5
Anton Novojilov <andy@essentialkaos.com>
2023-04-19 01:13:26 +0300

    Fix typos

--- app/cli.go
+++ app/cli.go
@@ -204,7 +204,7 @@ func validateConfig() {
 	}
 }
 
-// setupLogger confugures logger subsystems
+// setupLogger configures logger subsystems
 func setupLogger() {
 	err := log.Set(knf.GetS(LOG_FILE), knf.GetM(LOG_PERMS, 644))
 
@@ -234,7 +234,7 @@ func printWarn(f string, a ...interface{}) {
 	fmtc.Fprintf(os.Stderr, "{y}"+f+"{!}\n", a...)
 }
 
-// printErrorAndExit print error mesage and exit with exit code 1
+// printErrorAndExit print error message and exit with exit code 1
 func printErrorAndExit(f string, a ...interface{}) {
 	printError(f, a...)
 	os.Exit(1)
